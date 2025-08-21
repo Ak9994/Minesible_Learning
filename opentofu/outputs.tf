cf6de18ec168d073bfcd51621b392abfc7b8c148
Ak9994 <163250688+Ak9994@users.noreply.github.com>
2025-08-21 13:42:57 +0530

    Update outputs.tf

---
+++ opentofu/outputs.tf
@@ -0,0 +1,19 @@
+output "ec2_ip" {
+  value = aws_instance.minecraft.public_ip
+}
+
+output "s3_bucket" {
+  value = var.minecraft_s3_bucket != null ? var.minecraft_s3_bucket : aws_s3_bucket.minecraft_saves[0].bucket
+}
+
+output "motd" {
+  value = var.motd
+}
+
+output "max_players" {
+  value = var.max_players
+}
+
+output "instance_type" {
+  value = var.instance_type
+}
